55fb4550253a3aefc8b800822ce296bce4df7b0e
Isabeau Rikkoert <110368@st-maartenscollege.nl>
2021-01-11 11:13:21 +0000

    woordenlijst

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-volgensmij is het gelukt lol
+import random
+
+list = ['dog', 'chocolatemilk', 'picknick', 'shoppingcart', 'computer', 'music', 'tea', 'sunrise', 'beach', 'baseball', 'fireball', 'party', 'goose', 'sad', 'shower', 'tears', 'fighting', 'naïve', 'daisy', 'flowers']
+print(random.choice(list))

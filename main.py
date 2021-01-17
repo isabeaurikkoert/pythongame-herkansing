450d79b31f3e697b08ee5573c8a8bf218419d575
Isabeau Rikkoert <110368@st-maartenscollege.nl>
2021-01-17 20:57:49 +0000

    streepjes lengte woord

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import random
 
 list = ['dog', 'chocolatemilk', 'picknick', 'shoppingcart', 'computer', 'music', 'tea', 'sunrise', 'beach', 'baseball', 'fireball', 'party', 'goose', 'sad', 'shower', 'tears', 'fighting', 'naïve', 'daisy', 'flowers']
+#print woord
 print(random.choice(list))
+woord = '_ ' * len(random.choice(list))
+print(woord)

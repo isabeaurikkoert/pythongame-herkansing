66e02e1bf1ce4c3728884401554d8fdeb8828f8b
Isabeau Rikkoert <110368@st-maartenscollege.nl>
2021-01-18 13:13:50 +0000

    streepjes verbeterd

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 
 list = ['dog', 'chocolatemilk', 'picknick', 'shoppingcart', 'computer', 'music', 'tea', 'sunrise', 'beach', 'baseball', 'fireball', 'party', 'goose', 'sad', 'shower', 'tears', 'fighting', 'naïve', 'daisy', 'flowers']
 #print woord
-print(random.choice(list))
-woord = '_ ' * len(random.choice(list))
+doggo = random.choice(list)
+print(str(doggo))
+woord = ' _ ' * len(str(doggo))
 print(woord)

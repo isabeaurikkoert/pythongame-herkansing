ec7f0f51520f97d4127ca977765e12209b37c687
Isabeau Rikkoert <110368@st-maartenscollege.nl>
2021-01-18 21:32:05 +0000

    basisspel af

--- main.py
+++ main.py
@@ -1,40 +1,50 @@
 import random
 import sys 
+import time
 list = ['dog', 'chocolatemilk', 'picknick', 'shoppingcart', 'computer', 'music', 'tea', 'sunrise', 'beach', 'baseball', 'fireball', 'party', 'goose', 'sad', 'shower', 'tears', 'fighting', 'naïve', 'daisy', 'flowers']
 
 #print woord
 doggo = random.choice(list)
 print(str(doggo))
-
+temp = " _ " * len(str(doggo))
 #Intro 
 naam = input("wat is je naam")
 print("hallo " + naam, "laten we galgje spelen :)")
 woord = ' _ ' * len(str(doggo))
+time.sleep(0.5)
 print(woord)
 geraden_letters = []
 fout_geraden_letters = []
-
+geraden_woorden = []
 
 tries = 10
 while tries > 0:
-    guess = input("raad een letter of woord")
-    if len(guess) == 1 and guess.isalpha():
-      if guess in geraden_letters:
-        print("oeps die heb je al geprobeerd")
-    elif len(guess) == 1 and guess.isalpha():
-        if guess not in doggo:
-          print("jammer die zit er niet in")
-          tries -= 1
-         print("je hebt nog", + tries, "pogingen")
-         fout_geraden_letters.append(guess)
-         geraden_letters.append(guess)
-         print("fout geraden letters", + fout_geraden_letters)
-    elif len(guess) == 1 and guess.isalpha():
-      if guess
+  guess = input("raad een letter of woord")
+  if len(guess) == 1 and guess.isalpha():
+    if guess in geraden_letters:
+      print("oeps die heb je al geprobeerd")
+    elif guess not in doggo:
+      print("jammer die zit er niet in")
+      tries -= 1
+      time.sleep(0.5)
+      print("je hebt nog", + tries, "pogingen")
+      fout_geraden_letters.append(guess)
+      geraden_letters.append(guess)
+      time.sleep(0.5)
+      print("fout geraden letters:")
+      print(fout_geraden_letters)
+    else:
+      print("goed geraden!")
+      geraden_letters.append(guess)
+      for i in range(0, len(str(doggo))):
+          if guess == doggo[i]:
+            temp = temp[:i] + guess + temp[i+1:]
+            print(temp)
+            time.sleep(0.5)
+            print("je hebt nog", + tries, "pogingen")
+            time.sleep(0.5)
+            print('fout geraden letters:')
+            print(fout_geraden_letters)
+   
 
-reactie = input("wil je nog een keer spelen?")
-while true:
-  if reactie == ja
-    break
-  elif reactie == nee
-    sys.exit("jammer, doeidoei")
+
